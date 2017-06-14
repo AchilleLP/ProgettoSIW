d507ba0ec44ad10304b97061829bbcef5f85409d
TheReygen <mario.bernacchia2@gmail.com>
2017-06-14 17:31:34 +0200

    edit

--- Galleria/src/main/java/it/uniroma3/Galleria/controller/MainController.java
+++ Galleria/src/main/java/it/uniroma3/Galleria/controller/MainController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import it.uniroma3.Galleria.model.Opera;
+
 @Controller
 public class MainController {
 	 // Login form
@@ -22,7 +24,8 @@ public class MainController {
 	  
 	  // Inserisci Opera
 	  @RequestMapping("/formOpera")
-	  public String formOpera() {
+	  public String formOpera(Model model) {
+		  model.addAttribute("opera",new Opera());
 	    return "formOpera";
 	  }
 	  
